0b5960b285ed2a5946f8afc080cdd038cf8b1d29
DJ Doena <karsten@yajirobo.de>
2018-01-12 12:34:24 +0100

    adapted copyright year

--- ToolBox/Properties/AssemblyInfo.cs
+++ ToolBox/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Doena Soft.")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("Copyright © Doena Soft. 2016 - 2017")]
+[assembly: AssemblyCopyright("Copyright © Doena Soft. 2016 - 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
